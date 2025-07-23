7c19e951ab7e4127258371134dd27d5356795583
Vikalp-Pandey <vikalp.pandey2004@gmail.com>
2025-07-23 14:04:12 +0530

    Task Detail View for detail of each Tasks in List is created

--- App/views.py
+++ App/views.py
@@ -1,6 +1,10 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView
+
 from .models import Task
 
 # Create your views here.
@@ -8,4 +12,9 @@ from .models import Task
 class TaskList(ListView):
     model=Task
     context_object_name="tasks"
+
+class TaskDetail(DetailView): 
+    model=Task
+    context_object_name="task" 
+    template_name="App/task_detail.html"
     
