804755dd37e5c5e82948553ef2b5436167b263cb
Vikalp-Pandey <vikalp.pandey2004@gmail.com>
2025-07-24 09:34:10 +0530

    Restrictedevery unauthenticated user from accessing any end point

--- App/views.py
+++ App/views.py
@@ -4,12 +4,22 @@ from django.urls import reverse_lazy
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView,UpdateView,DeleteView
-
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Task
 
 # Create your views here.
+class CustomLoginView(LoginView):
+    template_name='App/login.html'
+    fields="__all__"
+    redirect_authenticated_user=True
+   
+    # *Creating a custom success_url 
+    def get_success_url(self):
+        return reverse_lazy('tasks')
 
-class TaskList(ListView):
+
+class TaskList(LoginRequiredMixin,ListView):
     model=Task
     context_object_name="tasks"
 
@@ -18,20 +28,20 @@ class TaskDetail(DetailView):
     context_object_name="task" 
     template_name="App/task_detail.html"
     
-class TaskCreate(CreateView):
+class TaskCreate(LoginRequiredMixin,CreateView):
     model=Task
     fields="__all__"
     # fields=['title','description']
     success_url= reverse_lazy('tasks')
-    # ? Note: Django provides built ModelForms for all the models we created. 
-    # ?We can override the fields in modelForms by specifying fields attribute.
+    # *Note: Django provides built ModelForms for all the models we created. 
+    # *We can override the fields in modelForms by specifying fields attribute.
 
-class TaskUpdate(UpdateView):
+class TaskUpdate(LoginRequiredMixin,UpdateView):
     model=Task
     fields="__all__"
     success_url= reverse_lazy('tasks')
 
-class TaskDelete(DeleteView):
+class TaskDelete(LoginRequiredMixin,DeleteView):
     model=Task
     context_object_name='task'
     success_url=reverse_lazy('tasks')
