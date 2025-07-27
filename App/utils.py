49a9f4b1d1dafd33f028effdf5a8735055855638
Vikalp-Pandey <vikalp.pandey2004@gmail.com>
2025-07-27 12:44:21 +0530

    Utils file added to keep Countries timezone and their location

---
+++ App/utils.py
@@ -0,0 +1,7 @@
+COUNTRY_TIMEZONE_MAP = {
+    'India': 'Asia/Kolkata',
+    'United States': 'America/New_York',
+    'United Kingdom': 'Europe/London',
+    'Australia': 'Australia/Sydney',
+    # Add more countries as needed
+}
