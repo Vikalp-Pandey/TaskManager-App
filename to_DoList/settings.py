e72ce81404d3179e20d063e65ca266ee474acc52
Vikalp-Pandey <vikalp.pandey2004@gmail.com>
2025-08-12 17:23:29 +0530

    Updated Installed Apps

--- to_DoList/settings.py
+++ to_DoList/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "App"
+    "App",
+    "django_celery_results",  # For storing Celery task results
 ]
 
 MIDDLEWARE = [
